96303dd32b03263a67186633a93704a144929abb
Lidiia <LIM00039@laurea.fi>
2026-09-10 14:39:19 +0300

    js and css

---
+++ WS02_JavaScript/js/script.js
@@ -0,0 +1,15 @@
+console.log ("Hello, World!");
+const userName = "Lidia";
+const favoriteAnimal = "Frog";
+console.log("My name is " + userName + " and my favorite animal is " + favoriteAnimal)
+console.log("my favorite animal is " + favoriteAnimal)
+//Display a pop-up message
+alert("Tervetuloa hienoulle nettisivylleni")
+
+//Ask user for their name
+const visitorName = prompt("What is your name?")
+console.log(visitorName);
+console.log("Terve " + visitorName + "! Tervetuloa JavaScriptiin paariin!");
+
+const visitorFavoriteAnimal = promt("What is your favorite animal?");
+
