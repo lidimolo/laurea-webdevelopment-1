28f95c190b973629ca344087c93e33fee9b534cb
Lidiia <LIM00039@laurea.fi>
2026-09-17 15:23:37 +0300

    java script

--- WS02_JavaScript/js/script.js
+++ WS02_JavaScript/js/script.js
@@ -1,7 +1,8 @@
 console.log ("Hello, World!");
-const userName = "Lidia";
-const favoriteAnimal = "Frog";
-console.log("My name is " + userName + " and my favorite animal is " + favoriteAnimal)
+const userName = "Lidiia";
+const favoriteAnimal = "owl";
+const userAge = "23";
+console.log("My name is " + userName + " I'm " + userAge + " and my favorite animal is " + favoriteAnimal)
 console.log("my favorite animal is " + favoriteAnimal)
 //Display a pop-up message
 alert("Tervetuloa hienoulle nettisivylleni")
@@ -11,5 +12,24 @@ const visitorName = prompt("What is your name?")
 console.log(visitorName);
 console.log("Terve " + visitorName + "! Tervetuloa JavaScriptiin paariin!");
 
-const visitorFavoriteAnimal = promt("What is your favorite animal?");
+function greetUser(visitorName) { console.log("Hello " + visitorName+ "!"); }
 
+greetUser(visitorName);
+
+const visitorFavoriteAnimal = prompt("What is your favorite animal?");
+console.log("Your favorite animal is " + visitorFavoriteAnimal);
+
+const visitorAge = prompt ("What is your age?");
+console.log(visitorAge);
+
+if (visitorAge >= 18) { 
+    console.log ("You are an adult");
+} else {
+    console.log("You are under 18.")
+}
+
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+    alert("JavaScript works!");
+});
